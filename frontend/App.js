0a870038ac74d224995f21647ecda3dac965f17d
Muzriaz003 <143139572+Muzriaz003@users.noreply.github.com>
2025-04-08 20:51:19 -0400

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h2>React Component Loaded</h2>
+      <p>This is the Cypress React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
